d7353c2df7aada9982aafbc55f64d17c5b32fbd6
Steven A. Matias Perez <54132314+abdiel44@users.noreply.github.com>
2021-07-03 00:56:59 -0400

    Update portfolio/src/main/java/com/google/sps/servlets/contactMeServlet.java
    Co-authored-by: Aljon Pineda <aljonjbpineda@gmail.com>

--- portfolio/src/main/java/com/google/sps/servlets/contactMeServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/contactMeServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/form-submit")
-public class contactMeServlet extends HttpServlet {
+public class ContactMeServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
